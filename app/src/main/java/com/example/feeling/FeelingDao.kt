ff36fa2596196f82b04e9596f515df188a5667e4
ruenfeng <limrf-wm17@student.tarc.edu.my>
2019-12-09 12:08:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/feeling/FeelingDao.kt
@@ -0,0 +1,15 @@
+package com.example.feeling
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface FeelingDao {
+    @Insert
+    suspend fun insertFeeling(feeling:Feeling)
+
+    @Query("SELECT * FROM feeling")
+    fun getFeelings(): LiveData<Feeling>
+}
